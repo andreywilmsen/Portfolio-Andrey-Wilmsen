540da542559c07351a28cb0cf1866b2498fd421a
andreywilmsen <andreywilmsendepaula@gmail.com>
2025-06-09 16:59:51 -0300

    atualizacao responsividade h1 e h2 seção hero

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -6,13 +6,14 @@ export default function Hero() {
 
     return (
         <section className="flex flex-col justify-center items-center w-full bg-section-primary h-dvh">
-            <div className=" flex justify-center items-center w-[600px] flex-col">
-                <h1 className="flex gap-x-4 items-center text-[64px] mb-1 h-[118px]">
+            <div className=" flex justify-center items-center w-full max-w-[600px] flex-col">
+                <h1 className="text-[40px] flex gap-x-2 text-center md:text-[64px] mb-2">
                     <span className="text-primary">ANDREY </span>
                     <span className="text-highlight ">WILMSEN</span>
                 </h1>
-                <h2 className="text-text-secondary mb-8 text-2xl">Desenvolvedor Fullstack - Node.Js / React.Js</h2>
-                <div className="flex flex-row justify-between md:justify-center items-center h-24 w-[128px]">
+                <h2 className="text-[16px] text-text-secondary mb-6 text-center md:text-2xl">
+                    Desenvolvedor Fullstack - Node.Js / React.Js</h2>
+                <div className="flex gap-4 justify-center items-center mt-4">
                     <div>
                         <Button source="github.png" name="Github" color="bg-github" />
                     </div>
