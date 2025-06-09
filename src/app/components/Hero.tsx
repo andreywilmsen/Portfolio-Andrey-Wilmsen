fa6e9d5e47d56c841d594a033a64cf1a0e6ac395
andreywilmsen <andreywilmsendepaula@gmail.com>
2025-06-09 19:53:35 -0300

    versão 1 hero concluída

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,25 +1,19 @@
 "use client"
 
-import Button from "./Button"
+import Button from "./Button";
+import TitleHero from "./TitleHero";
+import CaptionHero from "./CaptionHero";
 
 export default function Hero() {
 
     return (
-        <section className="flex flex-col justify-center items-center w-full bg-section-primary h-dvh">
-            <div className=" flex justify-center items-center w-full max-w-[600px] flex-col">
-                <h1 className="text-[40px] flex gap-x-2 text-center md:text-[64px] mb-2">
-                    <span className="text-primary">ANDREY </span>
-                    <span className="text-highlight ">WILMSEN</span>
-                </h1>
-                <h2 className="text-[16px] text-text-secondary mb-6 text-center md:text-2xl">
-                    Desenvolvedor Fullstack - Node.Js / React.Js</h2>
-                <div className="flex gap-4 justify-center items-center mt-4">
-                    <div>
-                        <Button source="github.png" name="Github" color="bg-github" />
-                    </div>
-                    <div>
-                        <Button source="linkedin.png" name="Linkedin" color="bg-linkedin" />
-                    </div>
+        <section className="bg-section-primary flex flex-col justify-center items-center w-full min-h-screen">
+            <div className="flex flex-col justify-center items-center max-w-[600px] w-[90%]">
+                <TitleHero />
+                <CaptionHero />
+                <div className="flex justify-center items-center gap-4 mt-4">
+                    <Button source="github.png" name="Github" color="bg-github" href="https://github.com/andreywilmsen" />
+                    <Button source="linkedin.png" name="Linkedin" color="bg-linkedin" href="https://www.linkedin.com/in/andrey-wilmsen-de-paula-2b1354345/" />
                 </div>
             </div>
         </section>
---
+++ src/app/components/TitleHero.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+export default function TitleHero() {
+
+    return (
+        <h1 className="flex text-[28px] sm:text-[36px] md:text-[64px] gap-x-2 text-center mb-2">
+            <span className="text-primary">ANDREY </span>
+            <span className="text-highlight ">WILMSEN</span>
+        </h1>
+    )
+}
