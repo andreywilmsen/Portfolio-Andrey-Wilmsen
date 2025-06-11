cc8dd01de8a10752e97d3a171d782e511be32765
andreywilmsen <andreywilmsendepaula@gmail.com>
2025-06-11 11:20:11 -0300

    componente da seção criada

---
+++ src/app/components/Projects/Projects.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+export default function Projects() {
+    return (
+        <section className="bg-section-secondary flex flex-col justify-center items-center w-full min-h-screen">
+            <div className="w-5/6 md:w-2/3 lg:w-3/6 xl:w-2/6">
+                <h1 className="text-secondary my-3 text-[32px] md:text-[34px] font-medium">
+                    <span className="border-b-2 border-b-highlight pb-2">PROJETOS</span>
+                </h1>
+            </div>
+        </section>
+    )
+}
