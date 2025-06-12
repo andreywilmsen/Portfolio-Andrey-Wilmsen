8bed252e20301bdfdb83d7c0ad8fb33c52119250
andreywilmsen <andreywilmsendepaula@gmail.com>
2025-06-12 20:31:20 -0300

    ajusta o paragrafo de sobre, titulo do site, descrição e favicon.

--- src/app/components/About/AboutParagraph.tsx
+++ src/app/components/About/AboutParagraph.tsx
@@ -3,7 +3,8 @@
 export default function AboutParagraph() {
     return (
         <p className="text-secondary font-normal text-[14px] md:text-[20px]">
-            Sou um desenvolvedor Fullstack com experiência em <span className="text-highlight">Node.js, MongoDB, SQL, React, HTML e CSS</span>. Apaixonado por tecnologia e pela criação de soluções eficientes, foco no desenvolvimento backend e frontend, otimização de sistemas e implementação de APIs REST. Sempre buscando aprimorar habilidades e contribuir para projetos inovadores.
+            Sou um desenvolvedor Fullstack com experiência em <span className="text-highlight">Node.js, Next.js, React, TypeScript, Tailwind, MongoDB, SQL e testes com Jest e TDD.
+            </span>. Apaixonado por tecnologia e pela criação de soluções eficientes, foco no desenvolvimento backend e frontend, otimização de sistemas e implementação de APIs REST. Sempre buscando aprimorar habilidades e contribuir para projetos inovadores.
         </p>
     )
 }
